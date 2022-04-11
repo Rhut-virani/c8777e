85092c9317716ce67169f82289ee39a9c5f91519
Rhut Virani <rhut.virani@gmail.com>
2022-04-11 08:31:06 -0400

    Revert "Database setup"
    This reverts commit d531b0fe61b8966c5a6e4ffc3012402938ce6dec.

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:5432/messenger`, {
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
 
